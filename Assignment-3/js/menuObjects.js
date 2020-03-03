709d339f015aa00040a78e7de57b851b3d24c0b4
morerebe <59507960+morerebe@users.noreply.github.com>
2020-03-03 13:20:01 -0500

    Add files via upload

---
+++ Assignment-3/js/menuObjects.js
@@ -0,0 +1,101 @@
+const menus = [
+  {
+    title: "Menu 1",
+    soup: {
+      name: "Roasted Red Pepper",
+      price: 4.0
+    },
+    salad: {
+      name: "Quinoa Salad",
+      price: 4.25
+    },
+    lighterFare: {
+      name: "Buffalo Cauliflower Wrap",
+      price: 5.0
+    },
+    entree: {
+      name: "Black Bean Burger & Fries",
+      price: 8.0
+    }
+  },
+
+  {
+    title: "Menu 2",
+    soup: {
+      name: "Mexican Black Bean",
+      price: 4.0
+    },
+    salad: {
+      name: "Spicy Slaw",
+      price: 4.25
+    },
+    lighterFare: {
+      name: "Chips, Salsa & Dip",
+      price: 5.0
+    },
+    entree: {
+      name: "Burrito Bowl",
+      price: 8.0
+    }
+  },
+
+  {
+    title: "Menu 3",
+    soup: {
+      name: "Vegetable Stew",
+      price: 4.0
+    },
+    salad: {
+      name: "Classic Caesar",
+      price: 4.25
+    },
+    lighterFare: {
+      name: "Grilled Veg Panini",
+      price: 5.0
+    },
+    entree: {
+      name: "Stuffed Sweet Potato",
+      price: 8.0
+    }
+  },
+
+  {
+    title: "Menu 4",
+    soup: {
+      name: "Cream of Asparagus",
+      price: 4.0
+    },
+    salad: {
+      name: "Greek Salad",
+      price: 4.25
+    },
+    lighterFare: {
+      name: "Warm Olives and Baguette",
+      price: 5.0
+    },
+    entree: {
+      name: "Tofu Souvlaki",
+      price: 8.0
+    }
+  },
+
+  {
+    title: "Menu 5",
+    soup: {
+      name: "Tomato Soup",
+      price: 4.0
+    },
+    salad: {
+      name: "Avocado Salad",
+      price: 4.25
+    },
+    lighterFare: {
+      name: "Chickpea Salad Sandwich",
+      price: 5.0
+    },
+    entree: {
+      name: "Vegan Mac & Cheese",
+      price: 8.0
+    }
+  }
+];
