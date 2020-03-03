5e16b7a4de4901ef980d7db6c604a6dec297c66a
morerebe <59507960+morerebe@users.noreply.github.com>
2020-03-03 13:25:14 -0500

    Add files via upload

--- Assignment-3/js/menuObjects.js
+++ Assignment-3/js/menuObjects.js
@@ -3,19 +3,19 @@ const menus = [
     title: "Menu 1",
     soup: {
       name: "Roasted Red Pepper",
-      price: 4.0
+      price: "4.00"
     },
     salad: {
       name: "Quinoa Salad",
-      price: 4.25
+      price: "4.25"
     },
     lighterFare: {
       name: "Buffalo Cauliflower Wrap",
-      price: 5.0
+      price: "5.00"
     },
     entree: {
       name: "Black Bean Burger & Fries",
-      price: 8.0
+      price: "8.00"
     }
   },
 
@@ -23,19 +23,19 @@ const menus = [
     title: "Menu 2",
     soup: {
       name: "Mexican Black Bean",
-      price: 4.0
+      price: "4.00"
     },
     salad: {
       name: "Spicy Slaw",
-      price: 4.25
+      price: "4.25"
     },
     lighterFare: {
       name: "Chips, Salsa & Dip",
-      price: 5.0
+      price: "5.00"
     },
     entree: {
       name: "Burrito Bowl",
-      price: 8.0
+      price: "8.00"
     }
   },
 
@@ -43,19 +43,19 @@ const menus = [
     title: "Menu 3",
     soup: {
       name: "Vegetable Stew",
-      price: 4.0
+      price: "4.00"
     },
     salad: {
       name: "Classic Caesar",
-      price: 4.25
+      price: "4.25"
     },
     lighterFare: {
       name: "Grilled Veg Panini",
-      price: 5.0
+      price: "5.00"
     },
     entree: {
       name: "Stuffed Sweet Potato",
-      price: 8.0
+      price: "8.00"
     }
   },
 
@@ -63,19 +63,19 @@ const menus = [
     title: "Menu 4",
     soup: {
       name: "Cream of Asparagus",
-      price: 4.0
+      price: "4.00"
     },
     salad: {
       name: "Greek Salad",
-      price: 4.25
+      price: "4.25"
     },
     lighterFare: {
       name: "Warm Olives and Baguette",
-      price: 5.0
+      price: "5.00"
     },
     entree: {
       name: "Tofu Souvlaki",
-      price: 8.0
+      price: "8.00"
     }
   },
 
@@ -83,19 +83,19 @@ const menus = [
     title: "Menu 5",
     soup: {
       name: "Tomato Soup",
-      price: 4.0
+      price: "4.00"
     },
     salad: {
       name: "Avocado Salad",
-      price: 4.25
+      price: "4.25"
     },
     lighterFare: {
       name: "Chickpea Salad Sandwich",
-      price: 5.0
+      price: "5.00"
     },
     entree: {
       name: "Vegan Mac & Cheese",
-      price: 8.0
+      price: "8.00"
     }
   }
 ];
